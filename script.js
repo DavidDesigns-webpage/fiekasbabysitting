cd9940db3c77a5f2a42e6fd8fca8364e9afa384d
breadSlice-bake <charles.mitsu@gmail.com>
2025-12-21 20:57:25 +0200

    Initial client website upload

---
+++ script.js
@@ -0,0 +1,30 @@
+const navButton = document.querySelector(".navButton");
+const nav = document.querySelector(".mobileNav");
+const sctollToTop = document.querySelector(".scrollToTop");
+
+navButton.addEventListener("click",(event) =>{
+    event.stopPropagation();
+    nav.classList.toggle("show");
+});
+nav.addEventListener("click",(event) =>{
+    event.stopPropagation();
+});
+document.addEventListener("click",()=>{
+    nav.classList.remove("show");
+});
+
+window.addEventListener("scroll",() =>{
+    if (window.scrollY > 300){
+        sctollToTop.classList.add("show");
+    }
+    else {
+        sctollToTop.classList.remove("show");
+    }
+});
+
+sctollToTop.addEventListener("click",()=>{
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+});
